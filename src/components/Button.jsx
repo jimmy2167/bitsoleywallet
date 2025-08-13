d933333a337444a6ea74ea82703302457afbcaba
BitGourde Project <87956155+jimmy2167@users.noreply.github.com>
2025-08-13 04:00:12 -0400

    Delete src directory

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import cn from "classnames";
-export default function Button({ children, className="", variant="solid", ...props }) {
-  const base = variant === "solid" ? "btn" : "btn-outline";
-  return (
-    <button className={cn(base, "disabled:opacity-50", className)} {...props}>
-      {children}
-    </button>
-  );
-}
